cbd8fb1606a7ab90337c855bffe93107039c0cc9
Hu Edward <hu_jiyu@icloud.com>
2021-04-30 09:55:05 -0500

    the best we can do

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,85 +28,29 @@ import 'chartkick/chart.js'
 function App() {
 
 
-  const sampleListOfStock = [
-    {
-      symbol: "AAPL",
-      company: "Apple",
-      Price: "5.0",
-      ListOfHistoryPrice:[
-        {price: "1.0"},
-        {price: "1.2"},
-        {price: "1.4"},
-        {price: "1.6"},
-        {price: "1.8"}
-      ]
-    },
-    {
-      symbol: "GGGGG",
-      company: "Apple",
-      Price: "5.0",
-      ListOfHistoryPrice:[
-        {price: "1.0"},
-        {price: "1.2"},
-        {price: "1.4"},
-        {price: "1.6"},
-        {price: "1.8"}
-      ]
-    },
-    {
-      symbol: "BBBBB",
-      company: "Apple",
-      Price: "5.0",
-      ListOfHistoryPrice:[
-        {price: "1.0"},
-        {price: "1.2"},
-        {price: "1.4"},
-        {price: "1.6"},
-        {price: "1.8"}
-      ]
-    }
-  ]
-  const sampleRecommendStock = [
-    {
-      symbol: "FUCK",
-      Price: "5.0"
-    },
-    {
-      symbol: "GGGGG",
-      Price: "5.0"
-    },
-    {
-      symbol: "BBBBB",
-      Price: "5.0"
-    },
-    {
-    symbol: "GGGGG",
-    Price: "5.0"
-  },
-  {
-    symbol: "BBBBB",
-    Price: "5.0"
-  }
-  ]
-  const sampleRecentView = ["AAPL"];
 
-  const [shit, setShit] = useState("I am shit");
+
+
+
   const [openRecent, setOpenRecent] = React.useState(false);
-  const [recentView, setRecentView] = React.useState(sampleRecentView);
+  const [recentView, setRecentView] = React.useState([]);
   const [openfavorites, setOpenfavorites] = React.useState(false);
   const [favorites, setfavorites] = React.useState([]);
-  const [openIndustry, setOpenIndustry] = React.useState(false);
-  const [checkPublishing, setCheckPublishing] = React.useState(false);
-  const [checkEntertainment, setCheckEntertainment] = React.useState(false);
+  const [openSector, setOpenSector] = React.useState(false);
+  const [sector, setSector] = React.useState([]);
+  const [sectorCheckList, setSectorCheckList] = React.useState([]);
+
   const [selectedStock, setSelectedStock] = React.useState("");
-  const [listOfStock, setListOfStock] = React.useState(sampleListOfStock);
-  const [listOfHistory, setListOfHistory] = React.useState(sampleListOfStock);
-  const [listOfIndustry, setListOfIndustry] = React.useState(sampleListOfStock);
-  const [recommendStock, setRecommendStock] = React.useState(sampleRecommendStock);
+  const [listOfStock, setListOfStock] = React.useState([]);
+  const [listOfHistory, setListOfHistory] = React.useState([]);
+  const [listOfIndustry, setListOfIndustry] = React.useState([]);
+  const [recommendStock, setRecommendStock] = React.useState([]);
   const [inputUsername, setInputUsername] = React.useState("");
   const [inputPassword, setInputPassword] = React.useState("");
   const [inputSearch, setInputSearch] = React.useState("");
   const [username, setUsername] = React.useState("");
+  const [userid, setUserid] = React.useState("");
+  const [historyData, setHistoryData] = React.useState([])
 
   const useStyles = makeStyles((theme) => ({
 
@@ -153,34 +97,26 @@ function App() {
   var handleLogin = () => {
     fetch("http://localhost:4000/login?username="+inputUsername+"&password="+inputPassword)
     .then(resp => resp.json()).then(resp=>{
-        setUsername(resp.message)
+        setUsername(resp.message[0]["account_name"])
+        setUserid(resp.message[0]["user_id"])
     }).catch(err=>console.log(err))
   };
 
-  var handleSearch = () => {
-    fetch("http://localhost:4000/search?searchquery="+inputSearch)
-    .then(resp => resp.json()).then(resp=>{
-        setListOfStock(resp.message)
-    }).catch(err=>console.log(err))
-  };
+
 
   var handleFilter = (callback) => {
 
     var fitersQuery = ""
-    var listOfFilter = [];
-    if (checkPublishing) { listOfFilter.push("Publishing") }
-    if (checkEntertainment) { listOfFilter.push("Entertainment") }
-    if (listOfFilter){
+    if (sectorCheckList){
       fitersQuery = "filterList[]="
     }
-    fitersQuery += listOfFilter.join("&filterList[]=");
-
+    fitersQuery += sectorCheckList.join("&filterList[]=");
     callback(fitersQuery);
 
   };
 
-  var handleFilterOut = (inputSearch) => {
-    fetch("http://localhost:4000/filter?"+inputSearch)
+  var handleFilterOut = (input) => {
+    fetch("http://localhost:4000/filter?"+input+"&searchquery="+inputSearch)
     .then(resp => resp.json()).then(resp=>{
         setListOfStock(resp.message)
     }).catch(err=>console.log(err))
@@ -188,44 +124,196 @@ function App() {
 
 
   var handleVisit = () =>{
-    fetch("http://localhost:4000/visit?visitedstock="+selectedStock)
+    console.log("selectedStock: " + selectedStock)
+    fetch("http://localhost:4000/visit?visitedstock="+selectedStock+"&user_id="+userid)
     .then(resp => resp.json()).then(resp=>{
-        setRecentView(resp.message);
+      if (userid){
+        setRecentView(recentView => [...recentView, selectedStock])
+      }
         setSelectedStock("");
     }).catch(err=>console.log(err))
   };
 
-  var handleHistory = () =>{
-    fetch("http://localhost:4000/history?selectedstock="+selectedStock)
+  var getVisit = () =>{
+
+    fetch("http://localhost:4000/getvisit?&user_id="+userid)
     .then(resp => resp.json()).then(resp=>{
-        setListOfHistory(resp.message)
+
+        var views = [];
+        for (const element of resp.message){
+          views.push(element.symbol)
+        }
+        console.log(views)
+        setRecentView(views);
     }).catch(err=>console.log(err))
   };
 
+
   var handleFavorite = () =>{
     console.log("Handling favorite")
-    if (favorites.indexOf(selectedStock) !== -1){
-      setfavorites(favorites.filter(item => item !== selectedStock))
-    }else{
-      setfavorites(favorites => [...favorites, selectedStock])
-    }
+    console.log("selectedStock: " + selectedStock)
+    fetch("http://localhost:4000/favorite?visitedstock="+selectedStock+"&user_id="+userid)
+    .then(resp => resp.json()).then(resp=>{
+      if (favorites.indexOf(selectedStock) !== -1){
+        setfavorites(favorites.filter(item => item !== selectedStock))
+      }else{
+        setfavorites(favorites => [...favorites, selectedStock])
+      }
+      advanced();
+    }).catch(err=>console.log(err))
   };
+
+  var advanced = () =>{
+    console.log("advanced")
+    fetch("http://localhost:4000/advanced?selectedStock="+selectedStock)
+    .then(resp => resp.json()).then(resp=>{
+      handleGetRecommand()
+    }).catch(err=>console.log(err))
+  };
+
+  var getFavorite = () =>{
+    fetch("http://localhost:4000/getfavorite?&user_id="+userid)
+    .then(resp => resp.json()).then(resp=>{
+        console.log("getting favorite")
+        var views = [];
+        for (const element of resp.message){
+          views.push(element.symbol)
+        }
+        console.log(views)
+        setfavorites(views);
+    }).catch(err=>console.log(err))
+  }
+
+  var handleGetRecommand = () => {
+    fetch("http://localhost:4000/getRecommand")
+    .then(resp => resp.json()).then(resp=>{
+      console.log("getting Recommand")
+        var views = [];
+        for (const element of resp.message){
+          views.push(element.symbol)
+        }
+        console.log("recommand:", views)
+        setRecommendStock(views);
+    }).catch(err=>console.log(err))
+  }
+
+
+  var handleSector = () => {
+    console.log("HandleSector")
+    fetch("http://localhost:4000/sector")
+    .then(resp => resp.json()).then(resp=>{
+      setSector(resp.message)
+    }).catch(err=>console.log(err))
+  }
+
+  var handleSectorCheckList = (item) => {
+    if (sectorCheckList.indexOf(item) !== -1){
+      setSectorCheckList(sectorCheckList.filter(stuff => stuff !== item))
+    }else{
+      setSectorCheckList(sectorCheckList => [...sectorCheckList, item])
+    }
+  }
+
+  var handleEnter =()=>{
+    fetch("http://localhost:4000/history?&symbol="+selectedStock)
+    .then(resp => resp.json()).then(resp=>{
+        setHistoryData(resp.message)
+    }).catch(err=>console.log(err))
+  }
 
   React.useEffect(() => {
     handleFilter(handleFilterOut);
-  }, [checkPublishing, checkEntertainment]);
+  }, [sectorCheckList]);
+
+  React.useEffect(() => {
+    handleSector();
+  }, []);
+
+  React.useEffect(() => {
+    if (userid){
+      getVisit();
+    }
+  }, [userid]);
+
+  React.useEffect(() => {
+    if (userid){
+      getFavorite();
+    }
+  }, [userid]);
+
+  React.useEffect(() => {
+    if (userid){
+      handleGetRecommand();
+    }
+  }, [userid]);
+
+  // React.useEffect(() => {
+  //   if (userid){
+  //     handleGetRecommand();
+  //   }
+  // }, [favorites]);
+
+
+
 
   return (
       <div className="App">
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
-      <Dialog onOpen={handleHistory} onClose={()=>{handleVisit() }} fullWidth maxWidth="500px" aria-labelledby="simple-dialog-title" open={selectedStock}>
+      <Dialog onEnter={handleEnter} onClose={handleVisit} fullWidth maxWidth="500px" aria-labelledby="simple-dialog-title" open={selectedStock}>
         <div  className={classes.dialogPaper}>
-         <DialogTitle id="simple-dialog-title">{selectedStock} History Price: </DialogTitle>
-          <LineChart data={listOfHistory} />
+         <DialogTitle id="simple-dialog-title">{selectedStock} </DialogTitle>
          <Button variant="outlined" color="primary" onClick={handleFavorite} className={classes.likeButton} >
           {favorites.indexOf(selectedStock) !== -1 ? <Icon color="primary">favorite</Icon> : <Icon color="primary">favorite_border</Icon>}
          </Button>
+         <div  className={classes.allowScrow}>
+         <ListItem>
+         <Grid container spacing={1}>
+
+           <Grid item className={classes.griditem} xs={1}/>
+           <Grid item className={classes.griditem} xs={2}>
+             Date
+           </Grid>
+           <Grid item className={classes.griditem} xs={2}>
+             Open Price
+           </Grid>
+           <Grid item className={classes.griditem} xs={2}>
+             Close Price
+           </Grid>
+           <Grid item className={classes.griditem} xs={2}>
+             Day High
+           </Grid>
+           <Grid item className={classes.griditem} xs={3}>
+             Day Low
+           </Grid>
+         </Grid>
+         </ListItem>
+         {historyData.map(item => {
+           return (
+             <ListItem>
+             <Grid container spacing={1}>
+
+               <Grid item className={classes.griditem} xs={1}/>
+               <Grid item className={classes.griditem} xs={2}>
+                 {item.Date_PK}
+               </Grid>
+               <Grid item className={classes.griditem} xs={2}>
+                 {item.Open_Price}
+               </Grid>
+               <Grid item className={classes.griditem} xs={2}>
+                 {item.Close_Price}
+               </Grid>
+               <Grid item className={classes.griditem} xs={2}>
+                 {item.Day_High}
+               </Grid>
+               <Grid item className={classes.griditem} xs={3}>
+                 {item.Day_Low}
+               </Grid>
+             </Grid>
+             </ListItem>
+           )
+         })}
+         </div>
          </div>
        </Dialog>
 
@@ -237,7 +325,7 @@ function App() {
         <TextField value={inputSearch} onChange={(e)=>{setInputSearch(e.target.value)}} fullWidth id="search" label="Search" variant="outlined" />
         </Grid>
         <Grid item className={classes.griditem} xs={1}>
-        <Button variant="outlined" color="primary" onClick={handleSearch} className={classes.comfirm}>
+        <Button variant="outlined" color="primary" onClick={()=>{handleFilter(handleFilterOut)}} className={classes.comfirm}>
          <Icon color="primary">done</Icon>
         </Button>
         </Grid>
@@ -247,7 +335,7 @@ function App() {
         </Grid>
 
         <Grid item className={classes.griditem} xs={2}>
-        <TextField value={inputPassword} onChange={(e)=>{setInputPassword(e.target.value)}} fullWidth id="password" label="Password" variant="outlined" />
+        <TextField type="password" value={inputPassword} onChange={(e)=>{setInputPassword(e.target.value)}} fullWidth id="password" label="Password" variant="outlined" />
         </Grid>
         <Grid item className={classes.griditem} xs={1}>
           <Button variant="outlined" color="primary" onClick={handleLogin} className={classes.comfirm}>
@@ -265,37 +353,30 @@ function App() {
               aria-labelledby="nested-list-subheader"
               className={classes.root}
             >
-              <ListItem button onClick={() => {setOpenIndustry(!openIndustry)}}>
+              <ListItem button onClick={() => {setOpenSector(!openSector)}}>
                 <ListItemText primary="Industry" />
-                {openIndustry ? <ExpandLess /> : <ExpandMore />}
+                {openSector ? <ExpandLess /> : <ExpandMore />}
               </ListItem>
-              <Collapse in={openIndustry} timeout="auto" unmountOnExit>
+              <Collapse in={openSector} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
-                  <ListItem button className={classes.nested}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={checkPublishing}
-                          onChange={() => {setCheckPublishing(!checkPublishing) }}
-                          inputProps={{ 'aria-label': 'primary checkbox' }}
+                  {sector.map(item =>{
+                    return(
+                      <ListItem button className={classes.nested}>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={sectorCheckList.indexOf(item.sector) !== -1}
+                              onChange={() => { handleSectorCheckList(item.sector) }}
+                              inputProps={{ 'aria-label': 'primary checkbox' }}
+                            />
+                          }
+                          label={item.sector}
                         />
-                      }
-                      label="Airlines"
-                    />
-                  </ListItem>
-                  <ListItem button className={classes.nested}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={checkEntertainment}
-                          onChange={() => {setCheckEntertainment(!checkEntertainment)}}
-                          inputProps={{ 'aria-label': 'primary checkbox' }}
-                        />
-                      }
-                      label="Insurance-Life"
-                    />
+                      </ListItem>
+                    )
+                  })}
 
-                  </ListItem>
+
                 </List>
               </Collapse>
             </List>
@@ -310,9 +391,19 @@ function App() {
           {listOfStock.map(item => {
             return (
               <ListItem button onClick={()=>{setSelectedStock(item.symbol)}}>
-                {" Symbol: " + item.symbol}
-                {" Industry: " + item.industry}
-                {" Price: " + item.market_price}
+              <Grid container spacing={1}>
+
+                <Grid item className={classes.griditem} xs={1}/>
+                <Grid item className={classes.griditem} xs={2}>
+                  {item.symbol}
+                </Grid>
+                <Grid item className={classes.griditem} xs={7}>
+                  {"("+item.company_name+")"}
+                </Grid>
+                <Grid item className={classes.griditem} xs={2}>
+                  {item.market_price}
+                </Grid>
+              </Grid>
               </ListItem>
             )
           })}
@@ -345,7 +436,7 @@ function App() {
                </List>
              </Collapse>
              <ListItem button onClick={() => {setOpenfavorites(!openfavorites)}}>
-               <ListItemText primary="favorites" />
+               <ListItemText primary="Favorites" />
                {openfavorites ? <ExpandLess /> : <ExpandMore />}
              </ListItem>
              <Collapse in={openfavorites} timeout="auto" unmountOnExit>
@@ -370,43 +461,38 @@ function App() {
 
         <Grid item className={classes.griditem} xs={1}/>
 
-        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[0].symbol)}}>
+        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[0])}}>
         <Paper >
           <div className={classes.bottomblock}>
-          {recommendStock[0].symbol}
-          {recommendStock[0].Price}
+          {recommendStock[0]}
           </div>
         </Paper>
         </Grid>
-        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[1].symbol)}}>
+        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[1])}}>
         <Paper >
           <div className={classes.bottomblock}>
-          {recommendStock[1].symbol}
-          {recommendStock[1].Price}
+          {recommendStock[1]}
           </div>
         </Paper>
         </Grid>
-        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[2].symbol)}}>
+        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[2])}}>
         <Paper>
           <div className={classes.bottomblock}>
-          {recommendStock[2].symbol}
-          {recommendStock[2].Price}
+          {recommendStock[2]}
           </div>
         </Paper>
         </Grid>
-        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[3].symbol)}}>
+        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[3])}}>
         <Paper>
           <div className={classes.bottomblock}>
-          {recommendStock[3].symbol}
-          {recommendStock[3].Price}
+          {recommendStock[3]}
           </div>
         </Paper>
         </Grid>
-        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[4].symbol)}}>
+        <Grid item className={classes.bottomblock} xs={2} button onClick= {()=>{setSelectedStock(recommendStock[4])}}>
         <Paper>
           <div className={classes.bottomblock}>
-          {recommendStock[4].symbol}
-          {recommendStock[4].Price}
+          {recommendStock[4]}
           </div>
         </Paper>
         </Grid>
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mysql = require("mysql");
 const app = express();
 app.use(cors())
 
-//TODO: you need to configure how to connect to DB here.
+USERNAME = ""
+
 var db = mysql.createConnection({
     host:'localhost',
     user: 'root',
@@ -18,37 +19,184 @@ app.get('/login', (req, res) => {
   console.log("I am about to login", req.query["username"])
   // TODO: please finish this part
   // If the username and password is correct, return with username as message. Else return a empty string in message
-  return res.json({message: req.query["username"]});
+  sqlSelect = "select user_id, account_name from user where account_name='" + req.query["username"] + "' and pwd='" + req.query["password"] + "'";
+  db.query(sqlSelect, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result)
+      return res.json({message: result});
+    }
+  });
+
 })
 
+app.get('/getvisit', (req, res)=>{
+  console.log("getting visit of", req.query["user_id"])
+  query = "select distinct symbol from view_history where user_id=" + req.query["user_id"]+ ";";
+  db.query(query, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result);
+      return res.json({message: result});
+    }
+  });
+})
 
 app.get('/visit', (req, res) => {
-  console.log("I am about to visit", req.query["selectedStock"])
+  console.log("I am about to visit", req.query["visitedstock"])
   // TODO: please finish this part
   // Return the new recent view list
-  const sampleNewRecentView = [{symbol:"UP"}, {symbol:"DA"}, {symbol:"TE"}, {symbol:"d"}];
-  return res.json({message: sampleNewRecentView});
+  if (req.query['user_id'] !== '' ){
+    query = "INSERT INTO view_history (user_id, symbol) " + "VALUES (" + req.query['user_id'] + ", '" + req.query['visitedstock'] + "')";
+    db.query(query, (err, result) => {
+        // response.send(result);
+      if (err) throw err;
+      else{
+        console.log(result);
+        return res.json({message: result});
+      }
+    });
+  }else{
+    return res.json({message: "sampleNewRecentView"});
+  }
+
+})
+
+app.get('/advanced', (req, res) => {
+  console.log("advanced", req.query['selectedStock'])
+  // TODO: please finish this part
+  // Return the new recent view list
+  query = "call recommend('" + req.query['selectedStock'] + "');";
+  db.query(query, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result);
+      return res.json({message: result});
+    }
+  });
+})
+
+app.get('/getfavorite', (req, res)=>{
+  console.log("getting visit of", req.query["user_id"])
+  query = "select distinct symbol from favorates where user_id=" + req.query["user_id"]+ ";";
+  db.query(query, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result);
+      return res.json({message: result});
+    }
+  });
+})
+
+app.get('/favorite', (req, res) => {
+  console.log("I am about to set favorite", req.query["visitedstock"])
+  // TODO: please finish this part
+  // Return the new recent view list
+  if (req.query['user_id'] !=='' && req.query['visitedstock'] !== undefined){
+    query = "INSERT INTO favorates (user_id, symbol) " + "VALUES (" + req.query['user_id'] + ", '" + req.query['visitedstock'] + "')";
+    db.query(query, (err, result) => {
+        // response.send(result);
+      if (err) throw err;
+      else{
+        console.log(result);
+        return res.json({message: result});
+      }
+    });
+  }
+  // return res.json({message: sampleNewRecentView});
+})
+
+app.get('/getRecommand', (req, res)=>{
+  console.log("getting recommand")
+  query = "select symbol from RecTable limit 5";
+  db.query(query, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result);
+      return res.json({message: result});
+    }
+  });
+})
+
+app.get('/history', (req, res)=>{
+  console.log("getting history of ", req.query["selectedStock"])
+  query = "select Date_PK, Day_High, Day_Low, Open_Price, Close_Price from historical_data where symbol='" + req.query["symbol"] + "'";
+  console.log(query)
+  db.query(query, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result);
+      return res.json({message: result});
+    }
+  });
+})
+
+app.get('/sector', (req, res) => {
+  // TODO: give me a list of sectors
+  sqlSelect = "select distinct sector from company;";
+  db.query(sqlSelect, (err, result) => {
+      // response.send(result);
+    if (err) throw err;
+    else{
+      console.log(result)
+      return res.json({message: result});
+    }
+  });
 })
 
 app.get('/filter', (req, res) => {
-  console.log("I am about to filtered", req.query["filterList"])
   // TODO: please finish this part
   // Return the filtered recent view list
   // const stockIndustry=["Airlines", "Tech"].toString();
   console.log("filter list: ", req.query["filterList"])
-  if (req.query["filterList"].length == 0){
+  console.log("searchquery: ", req.query["searchquery"])
+  if (req.query["filterList"][0] == ''){
     // If it is initailization, send back a string message
-    return res.json({message: "initailization"});
+    search = '';
+    if (req.query["searchquery"] !== ''){
+      search = "where c.company_name like '%" + req.query["searchquery"] + "%'";
+    }
+    sqlSelect = "SELECT s.symbol, s.market_price, c.company_name FROM stocks s natural join company c " + search + " LIMIT 100";
+    // console.log(sqlSelect)
+    db.query(sqlSelect, (err, result) => {
+        // response.send(result);
+      if (err) throw err;
+      else{
+        console.log(result)
+        return res.json({message: result});
+      }
+    });
   }else{
-
+    sqlSelect = "SELECT s.symbol, s.market_price, c.company_name FROM stocks s natural join company c where";
+    search = '';
+    limit = " LIMIT 100";
+    if (req.query["searchquery"] !== ''){
+      search = "c.company_name like '%" + req.query["searchquery"] + "%'";
+    }
+    if (req.query["filterList"]){
+      sqlSelect += " (sector = '"
+      if (search !== '')
+        search = "') and " + search;
+      else
+        limit = "')" + limit;
+    }
+    sqlSelect += req.query["filterList"].join("' or sector = '") + search + limit;
+    // console.log(sqlSelect)
+    db.query(sqlSelect, (err, result) => {
+        // response.send(result);
+      if (err) throw err;
+      else{
+        return res.json({message: result});
+      }
+    });
   }
-  const sqlSelect = "SELECT * FROM stocks WHERE symbol LIKE '%AA%'";
-  db.query(sqlSelect, (err, result) => {
-      // response.send(result);
-    console.log("SQL command: ", sqlSelect)
-    if (err) console.log(err);
-    return res.json({message: result});
-  });
+
 })
 
 app.get('/search', (req, res) => {
